c008db2378615b80f064654d82f6326a5363b31e
andresayac <andresayac@gmail.com>
2022-09-14 18:07:29 -0500

    change.php

---
+++ README.md
@@ -0,0 +1,17 @@
+# Holiday Validation
+Thanks to this scraper it is possible to validate if a day is a holiday in Spain, taking into account the province, all the information in this script is obtained from https://www.seg-social.es/wps/portal/wss/internet/CalendarioLaboral
+
+
+
+# Validación de dia feriado
+Gracias a este scraper es posible realizar la validación si un dia es feriado en España, teniendo en cuenta la provincia, toda la información de este script es obtenida desde https://www.seg-social.es/wps/portal/wss/internet/CalendarioLaboral
+
+## 📌 Installation
+```
+$ git clone https://github.com/andresayac/spain-holiday.git
+$ cd spain-holiday
+$ composer install
+```
+
+
+
--- app.php
+++ app.php
@@ -6,13 +6,26 @@ require 'inc/SegSocial.class.php';
 $scraper = new SegSocial();
 
 /*******************
- * get list of years available for validation
+ * get list of provinces available for validation
  */
-//$provinces = $scraper->getProvinces(); // return array
-//$locations = $scraper->getLocations($provinces); // return array
-//var_dump($provinces);
-//var_dump($locations);
+$provinces = $scraper->getProvinces(); // return array
+var_dump($provinces);
+
+/*******************
+ * get list of provinces available for validation
+ */
+$locations = $scraper->getLocations($provinces); // return array
+var_dump($locations);
+
+/*******************
+ * get list of holidays of the selected province
+ */
 
 $holidays = $scraper->getHolidays('28#Madrid', '280920000 #MOSTOLES');
-var_dump(json_encode($holidays));
+var_dump($holidays);
+
+
+/*******************
+ * Check if it is a holiday
+ */
 var_dump($scraper->isHoliday('2022-12-26', $holidays)); // return bool;
